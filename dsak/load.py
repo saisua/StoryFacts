31e9a7a9f20797bcfcf761465f28a05f627b199c
aus <aus@email.es>
2025-07-18 16:19:50 +0200

    Update dsak to allow multiline values

--- dsak/load.py
+++ dsak/load.py
@@ -11,6 +11,11 @@ def load(file_path: str) -> Generator[
 
 	lines = text.split("\n")
 
+	pending_key: str = None
+	pending_value: str = None
+
+	last_continue_line = False
+
 	args = list()
 	kwargs = dict()
 	for line in lines:
@@ -24,13 +29,39 @@ def load(file_path: str) -> Generator[
 			args.clear()
 			kwargs.clear()
 			continue
+		if line.endswith("\\"):
+			line = line[:-1]
 
-		splits = line.split(":", 1)
-		if len(splits) == 1:
-			args.append(line.strip())
+			if last_continue_line:
+				pending_value = f"{pending_value}\n{line.strip()}"
+			else:
+				splits = line.split(":", 1)
+				if len(splits) == 1:
+					pending_value = line.strip()
+				else:
+					pending_key, pending_value = splits
+					pending_key = pending_key.strip()
+					pending_value = pending_value.strip()
+			last_continue_line = True
+			continue
+
+		if last_continue_line:
+			if pending_key is None:
+				args.append(line.strip())
+				pending_value = None
+				last_continue_line = False
+			else:
+				kwargs[pending_key] = f"{pending_value}\n{line.strip()}"
+				pending_key = None
+				pending_value = None
+			last_continue_line = False
 		else:
-			key, value = splits
-			kwargs[key.strip()] = value.strip()
+			splits = line.split(":", 1)
+			if len(splits) == 1:
+				args.append(line.strip())
+			else:
+				key, value = splits
+				kwargs[key.strip()] = value.strip()
 
 	if args or kwargs:
 		yield args.copy(), kwargs.copy()
